d457dfbec541dfa96c7c33552f97b730ed9736a1
sebastiantorreslab <str9371@gmail.com>
2024-01-31 02:05:01 -0500

    letter frequency done alternative solution one for

--- src/main/java/com/springboot/code_challenge/algorithmics/strings/letterFrecuquency/Solution2.java
+++ src/main/java/com/springboot/code_challenge/algorithmics/strings/letterFrecuquency/Solution2.java
@@ -1,18 +1,20 @@
 package com.springboot.code_challenge.algorithmics.strings.letterFrecuquency;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Solution2 {
     public static void main(String[] args) {
-        System.out.println(repeatedLetters("solutionn"));
+        System.out.println(repeatedLetters("solutionn").stream().findFirst().orElse(String.valueOf(-1)));
     }
 
-    public static Set<String> repeatedLetters(String entry) {
+    public static List<String> repeatedLetters(String entry) {
         char[] sequence = entry.toCharArray();
 
         Set<String> letters = new HashSet<>();
-        Set<String> repeated = new HashSet<>();
+        List<String> repeated = new ArrayList<>();
 
         for (int i = 0; i < sequence.length; i++) {
             if (letters.contains(String.valueOf(sequence[i]))) {
