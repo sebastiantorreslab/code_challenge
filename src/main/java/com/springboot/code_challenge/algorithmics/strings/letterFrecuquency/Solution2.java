b0e08eefb29a51664aca9e862d0eb6edf2305a8f
sebastiantorreslab <str9371@gmail.com>
2024-01-31 02:25:47 -0500

    letter frequency done posible solved index of method

--- src/main/java/com/springboot/code_challenge/algorithmics/strings/letterFrecuquency/Solution2.java
+++ src/main/java/com/springboot/code_challenge/algorithmics/strings/letterFrecuquency/Solution2.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Solution2 {
     public static void main(String[] args) {
-        System.out.println(repeatedLetters("solutionn").stream().findFirst().orElse(String.valueOf(-1)));
+        String text = "bogota";
+        String value = repeatedLetters(text).get(0);
+        System.out.println(repeatedLetters(text));
+        System.out.println(new StringBuilder(text).indexOf(value));
     }
 
     public static List<String> repeatedLetters(String entry) {
@@ -16,6 +20,7 @@ public class Solution2 {
         Set<String> letters = new HashSet<>();
         List<String> repeated = new ArrayList<>();
 
+
         for (int i = 0; i < sequence.length; i++) {
             if (letters.contains(String.valueOf(sequence[i]))) {
                 repeated.add(String.valueOf(sequence[i]));
@@ -23,7 +28,7 @@ public class Solution2 {
                 letters.add(String.valueOf(sequence[i]));
             }
         }
-        return repeated;
+        return repeated.stream().sorted().collect(Collectors.toList());
     }
 }
 
