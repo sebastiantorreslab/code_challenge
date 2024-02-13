cbbf040af0c60e26463aacd37a95fce02f733d0b
sebastiantorreslab <str9371@gmail.com>
2024-02-12 23:46:27 -0500

    java minor element challenge done

--- src/main/java/com/springboot/code_challenge/algorithmics/numbers/smallestElementInArray/Solution.java
+++ src/main/java/com/springboot/code_challenge/algorithmics/numbers/smallestElementInArray/Solution.java
@@ -1,5 +1,6 @@
 package com.springboot.code_challenge.algorithmics.numbers.smallestElementInArray;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
@@ -7,17 +8,25 @@ public class Solution {
     // need to compare if the next number is minor than a first chosen element in the entry array.
 
     public static void main(String[] args) {
+        Integer[] nums = {10, 2, 3, 5, 10, 15,2};
+        List<Integer> numbers = new ArrayList<>(List.of(nums));
+
+        System.out.println(minorElement(numbers));
 
     }
 
-    public static int minorElement(List<Integer> elements){
+    public static Integer minorElement(List<Integer> elements) {
         boolean constraint = !elements.isEmpty();
-
-        for(int i=0;i<=elements.size();i++){
-
+        Integer minor = elements.get(0);
+        if (constraint) {
+            for (int i = 0; i < elements.size(); i++) {
+                if (elements.get(i) < minor && elements.get(i) >=0) {
+                    minor = elements.get(i);
+                }
+            }
+        } else {
+            System.out.println("empty list");
         }
-
-        return 0;
-
+        return minor;
     }
 }
