7f4e008e7c8f9dd5826594c890ee3e1e766c30ac
sebastiantorreslab <str9371@gmail.com>
2024-02-13 23:03:09 -0500

    java twoPrimesSum init

---
+++ src/main/java/com/springboot/code_challenge/algorithmics/numbers/twoPrimesSum/Solution.java
@@ -0,0 +1,14 @@
+package com.springboot.code_challenge.algorithmics.numbers.twoPrimesSum;
+
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Integer sumTwoPrimeNumbers(List<Integer> numbers){
+        return 0;
+    }
+}
