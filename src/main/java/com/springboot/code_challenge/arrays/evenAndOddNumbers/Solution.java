d8bb781b0be55fc686f795bccf7e9b5c2f59a48c
sebastiantorreslab <str9371@gmail.com>
2024-02-28 00:17:33 -0500

    remove oddNumbers update

--- src/main/java/com/springboot/code_challenge/arrays/evenAndOddNumbers/Solution.java
+++ src/main/java/com/springboot/code_challenge/arrays/evenAndOddNumbers/Solution.java
@@ -1,10 +1,14 @@
 package com.springboot.code_challenge.arrays.evenAndOddNumbers;
 
+import java.util.Arrays;
+
 public class Solution {
 
-    int[] numbers = {4, 5, 6, 8, 7, 4, 5, 6};
+    static int[] numbers = {4, 5, 6, 8, 7, 4, 5, 6};
 
     public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(removeOddNumbers(numbers)));
 
     }
 
@@ -13,15 +17,21 @@ public class Solution {
         for (int num : nums) {
             if (num % 2 != 0) {
                 numberCount++;
+            }
 
-            }
-            int[] oddNumbers = new int[numberCount];
-            for (int i = 0; i < oddNumbers.length; i++) {
-                if (nums[i] % 2 != 0) {
-                    oddNumbers[i] = nums[i];
-                }
-            }
-            return oddNumbers;
         }
+
+        int[] oddNumbers = new int[numberCount];
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                oddNumbers[counter++] = nums[i];
+            }
+        }
+
+
+
+        return oddNumbers;
     }
 }
