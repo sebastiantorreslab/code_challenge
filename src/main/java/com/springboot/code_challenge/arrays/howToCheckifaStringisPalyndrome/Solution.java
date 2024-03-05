916864ecca2c29cf388aa89feea2f4559ce0b969
sebastiantorreslab <str9371@gmail.com>
2024-03-05 13:53:45 -0500

    linkedlist package addeed

--- src/main/java/com/springboot/code_challenge/arrays/howToCheckifaStringisPalyndrome/Solution.java
+++ src/main/java/com/springboot/code_challenge/arrays/howToCheckifaStringisPalyndrome/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(isPalindrome("oso"));
+        System.out.println(isPalindrome("madam"));
 
     }
 
