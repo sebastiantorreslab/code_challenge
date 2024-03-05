0cf47a460a1611a7a16cef8335ccba5a8e00e299
sebastiantorreslab <str9371@gmail.com>
2024-03-05 11:55:07 -0500

    find minimun value in an array

--- src/main/java/com/springboot/code_challenge/arrays/finMinimunValue/Solution.java
+++ src/main/java/com/springboot/code_challenge/arrays/finMinimunValue/Solution.java
@@ -2,5 +2,29 @@ package com.springboot.code_challenge.arrays.finMinimunValue;
 
 public class Solution {
 
+    // fin the minimum value in a given array
+
+    public static void main(String[] args) {
+
+        int[] n = {10,2,4,5,8,7,5,6,6,3,2,1,54,6,6,5,0};
+
+        System.out.println("minimum value: " + findMinimumValueInArray(n));
+
+    }
+
+
+    public static int findMinimumValueInArray(int[] numbers){
+        int min = numbers[0];
+
+        for(int i =0; i < numbers.length; i++)
+            if(min > numbers[i]){
+                min = numbers[i];
+            }
+
+        return min;
+    }
+
+
+
 
 }
