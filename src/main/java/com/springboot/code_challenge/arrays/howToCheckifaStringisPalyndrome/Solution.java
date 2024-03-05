398d68e83864e59b0cf67444337f99a0a4c12a6e
sebastiantorreslab <str9371@gmail.com>
2024-03-05 12:12:39 -0500

    check if a string is a palyndrome

--- src/main/java/com/springboot/code_challenge/arrays/howToCheckifaStringisPalyndrome/Solution.java
+++ src/main/java/com/springboot/code_challenge/arrays/howToCheckifaStringisPalyndrome/Solution.java
@@ -2,6 +2,31 @@ package com.springboot.code_challenge.arrays.howToCheckifaStringisPalyndrome;
 
 public class Solution {
 
+    public static void main(String[] args) {
+
+        System.out.println(isPalyndrome("oso"));
+
+    }
+
+
+    public static boolean isPalyndrome(String text){
+
+        char[] sequence = text.toCharArray();
+        int n = sequence.length - 1;
+        boolean response = false;
+        for (char c : sequence) {
+            if (c == sequence[n]) {
+                response = true;
+            } else {
+                response = false;
+                return response;
+            }
+            n--;
+        }
+
+        return response;
+    }
+
 
 
 }
