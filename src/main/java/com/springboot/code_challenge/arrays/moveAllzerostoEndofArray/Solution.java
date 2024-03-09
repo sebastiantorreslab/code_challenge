cb6cc088b3ae72a67f8004c31b0c0a7133c63581
sebastiantorreslab <str9371@gmail.com>
2024-03-08 23:53:57 -0500

    init zeros relocation

--- src/main/java/com/springboot/code_challenge/arrays/moveAllzerostoEndofArray/Solution.java
+++ src/main/java/com/springboot/code_challenge/arrays/moveAllzerostoEndofArray/Solution.java
@@ -3,4 +3,13 @@ package com.springboot.code_challenge.arrays.moveAllzerostoEndofArray;
 public class Solution {
 
 
+    public static void main(String[] args) {
+
+    }
+
+    public static void relocateZeros(){
+
+    }
+
+
 }
