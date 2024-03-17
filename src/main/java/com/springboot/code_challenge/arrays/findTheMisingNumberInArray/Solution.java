a5ad631da747615eb2ffe329d51bce6c458fab1a
sebastiantorreslab <str9371@gmail.com>
2024-03-16 22:51:21 -0500

    init two new solutions

--- src/main/java/com/springboot/code_challenge/arrays/findTheMisingNumberInArray/Solution.java
+++ src/main/java/com/springboot/code_challenge/arrays/findTheMisingNumberInArray/Solution.java
@@ -2,6 +2,15 @@ package com.springboot.code_challenge.arrays.findTheMisingNumberInArray;
 
 public class Solution {
 
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int finMissingNumber(){
+
+    }
+
 
 
 }
